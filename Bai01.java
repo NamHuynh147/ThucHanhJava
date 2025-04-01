f9a99859b12c1d21a30d38ccc5574c7364d76840
NamHuynh <162312402+NamHuynh147@users.noreply.github.com>
2025-04-01 16:07:27 +0700

    Delete Bai01.java

--- Bai01.java
+++
@@ -1,9 +0,0 @@
-public class Bai01 {
-
-    public static void main(String[] args) throws Exception {
-
-        System.out.println("Ten :Huynh Dai Luc ");
-        System.out.println("Ngay sinh :16/05/2005");
-        System.out.println("MSSV :1250080109");
-    }
-}
